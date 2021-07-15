a262dd922907574aa854f638b55028ff8bc287aa
Architgarg1105 <architgarglmg9.4@gmail.com>
2021-07-15 16:42:23 +0530

    ajax calling

--- jsonifylas.py
+++ jsonifylas.py
@@ -2,17 +2,22 @@ from LAS import Converter
 from flask import Flask, request, jsonify, render_template, make_response
 import json
 import html
+from flask_cors import CORS
 
 app = Flask(__name__,  template_folder='')
-
+CORS(app)
 @app.route('/')
 def upload():
   return render_template("homepage.html")
+@app.route('/test',methods=['POST'])
+def test():
+  return jsonify('hello world')
 
-@app.route('/uploader', methods=['GET', 'POST'])
+@app.route('/uploader', methods=['POST'])
 def upload_file():
     if request.method == "POST":
         f = request.files['file']
+        print("hello")
 
         # read stream
         stream = f.stream.readlines()

0a8074e6f7ce3bdb460930d0a6f72aba759b9dc9
Architgarg1105 <architgarglmg9.4@gmail.com>
2021-07-16 00:21:00 +0530

    updated python file

--- jsonifylas.py
+++ jsonifylas.py
@@ -7,9 +7,9 @@ from flask_cors import CORS
 app = Flask(__name__,  template_folder='')
 CORS(app)
 @app.route('/')
-def upload():
-  return render_template("home2.html")
-  
+def home():
+  return render_template("homepage.html")
+
 # @app.route('/test',methods=['POST'])
 # def test():
 #   return jsonify('hello world')
@@ -18,22 +18,20 @@ def upload():
 def upload_file():
     if request.method == "POST":
         f = request.files['file']
-        print("hello")
-
         # read stream
-        stream = f.stream.readlines()
+        s = f.stream.readlines()
 
         # code for Las Converter
         c = Converter()
-        log  = c.set_stream(stream)
+        lasstreams  = c.set_stream(s)
 
         #save diffrent streams of LAS file
-        log_in_json=log.get_json()
+        lasdata=lasstreams.get_json()
 
         # or get only assci/data
         # data = log.data
 
-        return make_response(log_in_json,200)
+        return make_response(lasdata,200)
 
 if __name__ =='__main__':
 	app.run(debug=True)
